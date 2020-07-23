8185f07411593a80499920c3d772678897d3a469
Mieszko Boczkowski <mieszko.boczkowski@gmail.com>
2020-07-24 00:31:51 +0200

    typo

--- 03-pipeCustomClass/test/meshTest.C
+++ 03-pipeCustomClass/test/meshTest.C
@@ -29,7 +29,7 @@ int main(int argc, char *argv[])
 
     scalar avgTemp = getPatchAverage(T, outletPatch);
 
-    Info "Average temperature = " << avgTemp << endl;
+    Info << "Average temperature = " << avgTemp << endl;
     Info<< "Time = " << runTime.timeName() << endl;
 
     return 0;
