d7d82526c181436b0021d61572d71097bb8e98c4
Mieszko Boczkowski <mieszko.boczkowski@gmail.com>
2020-11-03 09:48:59 +0100

    Add PID algorithm implementation to Regulator

--- bndCondition/temperaturePIDController/regulatorLibrary/regulatorLibrary.C
+++ bndCondition/temperaturePIDController/regulatorLibrary/regulatorLibrary.C
@@ -1,27 +1,39 @@
 #include "regulatorLibrary.H"
 
-Regulator::Regulator(const fvMesh &mesh):
-    mesh_(mesh),
-    error_(0.), // TODO change initial value to lookupOrDefault("error", 0.)
-    errorIntegral_(0.),  // TODO as above
-    oldError_(0.),
-    timeIndex_(mesh.time().timeIndex())
+// * * * * * * * * * * * * Private Member Functions  * * * * * * * * * * * * //
+
+scalar Regulator::patchAverage(const word &fieldName, const fvPatch &patch) const
+{
+    const fvPatchField<scalar> &field =
+        patch.lookupPatchField<volScalarField, scalar>(fieldName);
+
+    return gSum(field * patch.magSf()) / gSum(patch.magSf());
+}
+
+// * * * * * * * * * * * * * * * * Constructor * * * * * * * * * * * * * * * //
+
+Regulator::Regulator(const fvMesh &mesh)
+    : mesh_(mesh),
+      error_(0.),         // TODO change initial value to lookupOrDefault("error", 0.)
+      errorIntegral_(0.), // TODO as above
+      oldError_(0.), timeIndex_(mesh.time().timeIndex())
 {
     // Get access to a custom dictionary
     const word dictName("regulatorProperties");
 
     // Create and input-output object - this holds the path to the dict and its name
-    IOobject dictIO(
-        dictName,               // name of the file
-        mesh.time().constant(), // path to where the file is
-        mesh,                   // reference to the mesh needed by the constructor
-        IOobject::MUST_READ     // indicate that reading this dictionary is compulsory
+    IOobject dictIO(dictName,               // name of the file
+                    mesh.time().constant(), // path to where the file is
+                    mesh,                   // reference to the mesh needed by the constructor
+                    IOobject::MUST_READ     // indicate that reading this dictionary
+                                            // is compulsory
     );
 
     // Check the if the dictionary is present and follows the OF format
     if (!dictIO.typeHeaderOk<dictionary>(true))
-        FatalErrorIn("regulatorLibrary.C") << "Cannot open specified refinement dictionary "
-                                           << dictName << exit(FatalError);
+        FatalErrorIn("regulatorLibrary.C")
+            << "Cannot open specified refinement dictionary " << dictName
+            << exit(FatalError);
     else
         Info << "Dictionary OK" << endl;
 
@@ -37,8 +49,10 @@ Regulator::Regulator(const fvMesh &mesh):
     D_ = regulatorDict_.getScalar("D");
 }
 
+// * * * * * * * * * * * * Public Member Functions  * * * * * * * * * * * * *//
+
 scalar Regulator::read()
-{       
+{
     Info << "Time index: " << mesh_.time().timeIndex() << endl;
     Info << "fieldName: " << fieldName_ << endl;
     Info << "patchName: " << patchName_ << endl;
@@ -57,5 +71,20 @@ scalar Regulator::read()
         oldError_ = error_;
     }
 
-    return 1.0;
+    // Get the target patch average field value
+    const fvPatch &targetPatch = mesh_.boundary()[patchName_];
+    const scalar targetPatchValue = patchAverage(fieldName_, targetPatch);
+
+    // Calculate errors
+    error_ = targetValue_ - targetPatchValue;
+    errorIntegral_ += 0.5*(error_ + oldError_)*deltaT;
+    const scalar errorDifferential = (error_ - oldError_) / deltaT;
+
+    // Calculate output signal
+    const scalar outputSignal = P_*error_ + I_*errorIntegral_ + D_*errorDifferential;
+
+    // Return result within defined SIGNAL_MIN and SIGNAL_MAX
+    const scalar result = max(min(outputSignal, REG_SIGNAL_MAX), REG_SIGNAL_MIN);
+
+    return result;
 }
