a53096dbaff98ed928c119c6efd9145edbee8766
Mieszko Boczkowski <mieszko.boczkowski@gmail.com>
2021-01-31 22:18:18 +0100

    Add fixed gradient convective heat flux bnd condition

---
+++ bndConditions/temperaturePIDFixedGradient/temperaturePIDFixedGradientFvPatchScalarField.C
@@ -0,0 +1,140 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | www.openfoam.com
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+    Copyright (C) 2011-2017 OpenFOAM Foundation
+    Copyright (C) 2020 OpenCFD Ltd.
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "temperaturePIDFixedGradientFvPatchScalarField.H"
+#include "addToRunTimeSelectionTable.H"
+#include "fvPatchFieldMapper.H"
+#include "volFields.H"
+#include "surfaceFields.H"
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::temperaturePIDFixedGradientFvPatchScalarField::temperaturePIDFixedGradientFvPatchScalarField
+(
+    const fvPatch& p,
+    const DimensionedField<scalar, volMesh>& iF
+)
+:
+    fixedGradientFvPatchScalarField(p, iF),
+    TName_("T")
+{}
+
+
+Foam::temperaturePIDFixedGradientFvPatchScalarField::temperaturePIDFixedGradientFvPatchScalarField
+(
+    const temperaturePIDFixedGradientFvPatchScalarField& ptf,
+    const fvPatch& p,
+    const DimensionedField<scalar, volMesh>& iF,
+    const fvPatchFieldMapper& mapper
+)
+:
+    fixedGradientFvPatchScalarField(ptf, p, iF, mapper),
+    TName_(ptf.TName_)
+{}
+
+
+Foam::temperaturePIDFixedGradientFvPatchScalarField::temperaturePIDFixedGradientFvPatchScalarField
+(
+    const fvPatch& p,
+    const DimensionedField<scalar, volMesh>& iF,
+    const dictionary& dict
+)
+:
+    fixedGradientFvPatchScalarField(p, iF),
+    TName_(dict.lookupOrDefault<word>("T", "T"))
+{
+    fvPatchField<scalar>::operator=(patchInternalField());
+    gradient() = 0.0;
+}
+
+
+Foam::temperaturePIDFixedGradientFvPatchScalarField::temperaturePIDFixedGradientFvPatchScalarField
+(
+    const temperaturePIDFixedGradientFvPatchScalarField& tppsf
+)
+:
+    fixedGradientFvPatchScalarField(tppsf),
+    TName_(tppsf.TName_)
+{}
+
+
+Foam::temperaturePIDFixedGradientFvPatchScalarField::temperaturePIDFixedGradientFvPatchScalarField
+(
+    const temperaturePIDFixedGradientFvPatchScalarField& tppsf,
+    const DimensionedField<scalar, volMesh>& iF
+)
+:
+    fixedGradientFvPatchScalarField(tppsf, iF),
+    TName_(tppsf.TName_)
+{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+void Foam::temperaturePIDFixedGradientFvPatchScalarField::updateCoeffs()
+{
+    if (updated())
+    {
+        return;
+    }
+
+    const fvPatchScalarField& T =
+        patch().lookupPatchField<volScalarField, scalar>(TName_);
+
+    const dictionary& transportProperties = db().lookupObject<IOdictionary>("transportProperties");
+
+    dimensionedScalar Tsur(transportProperties.lookup("Tsur"));
+    dimensionedScalar h(transportProperties.lookup("h"));
+    dimensionedScalar k(transportProperties.lookup("k"));
+
+    gradient() = - h.value() / k.value() * (T - Tsur.value());
+
+    fixedGradientFvPatchScalarField::updateCoeffs();
+}
+
+
+void Foam::temperaturePIDFixedGradientFvPatchScalarField::write(Ostream& os) const
+{
+    fvPatchScalarField::write(os);
+    os.writeEntryIfDifferent<word>("T", "T", TName_);
+    writeEntry("value", os);
+}
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+    makePatchTypeField
+    (
+        fvPatchScalarField,
+        temperaturePIDFixedGradientFvPatchScalarField
+    );
+}
+
+// ************************************************************************* //
